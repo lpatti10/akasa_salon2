e1c1456995548f697a9946954d613acd69231bd9
Laura Patti <lpatti10@gmail.com>
2014-07-04 11:51:08 -0400

    Photo blur and nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('The Iron Yard Rocks');
+// #Image Credit - https://medium.com/
+
+// #https://github.com/andreasstorm
+
+// $(window).scroll ->
+//   oVal = ($(window).scrollTop() / 240)
+//   $(".blur").css "opacity", oVal;
+
+
+(function() {
+  $(window).scroll(function() {
+    var oVal;
+    oVal = $(window).scrollTop() / 240;
+    return $(".blur").css("opacity", oVal);
+  });
+
+}).call(this);
